dab128fba4642a373789abcd2b885b00014bc9c6
alvesalan <38699636+alvesalan@users.noreply.github.com>
2018-06-05 15:55:56 -0300

    heroku

---
+++ ExercicioGetSet/ExercicioGetSet/Cabelo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioGetSet
+{
+    class Cabelo
+    {
+        private double TamanhoFio;
+        private double GrossuraFio;
+        private string CorFio;
+
+    
+
+    public void GetTamanho(double tamanhofio)
+    {
+        if (tamanhofio < 1)
+        {
+            throw new Exception("Tamanho do fio de cabelo não pode ser menor que 1 !!");
+        }
+        if (tamanhofio > 90)
+        {
+            throw new Exception("Tamanho do fio de cabelo não pode ser maior que 90 !!");
+        }
+        this.TamanhoFio = tamanhofio;
+    }
+        public void GetGrossuraFio(double grossurafio)
+    {
+        if (grossurafio < 0.1)
+        {
+            throw new Exception("Tamanho da grossura do fio de cabelo não pode ser menor que 1.0 !!");
+        }
+        if (grossurafio > 1.0)
+        {
+            throw new Exception("Tamanho da grossura do cabelo não pode ser maior que 1.0")
+        }
+            this.GrossuraFio = grossurafio;
+    }
+        public void GetCorFio(string corfio)
+        {
+            if (corfio.Count < 7)
+            {
+                throw new Exception("A quantidade de caracter da cor do fio de cabelo não pode ser menor que 7 !!");
+            }
+            if (corfio.Count > 20)
+            {
+                throw new Exception("A quantidade de caracter da cor do fio de cabelo não pode ser maior que 20 !!");
+            }
+            this.CorFio = corfio;
+        }
+       
+        
+
+    }
+}
---
+++ ExercicioGetSet/ExercicioGetSet/Casaco.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioGetSet
+{
+    class Casaco
+    {
+        private string Cor;
+        private double Preco;
+        private string Tamanho;
+
+        public void GetCor(string cor)
+        {
+            if (cor.Count < 7)
+            {
+                throw new Exception("Cor deve ter no minimo 7 caracter !!");
+            }
+            if (cor.Count > 20)
+            {
+                throw new Exception("Cor deve ter no maximo 20 caracter !!");
+            }
+
+            this.Cor = cor;
+        }
+        public void GetPreco(double preco)
+        {
+            if (preco < 10)
+            {
+                throw new Exception("Preço minimo e de 10 R$");
+            }
+            if (preco > 900)
+            {
+                throw new Exception("Preço maximo e de 900 R$");
+            }
+            this.Preco = preco;
+        }
+        public void GetTamanho(string tamanho)
+        {
+            if (tamanho.Count < 1)
+            {
+                throw new Exception("Quantidade minima para tamanho e de 1 caracter !!");
+            }
+            if (tamanho > 3)
+            {
+                throw new Exception("Quantidade maxima para tamanho e de 3 caracter !!");
+            }
+            this.Tamanho = tamanho;
+        }
+
+    }
+
+}
